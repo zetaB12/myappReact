6943b4af515e1065334a685fec8dfd1eddcfbb4b
Deyvi <deyvizavaleta@gmail.com>
2019-10-26 19:34:50 -0500

    started React

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from '../logo.svg';
+import '../css/App.css';
+import Header from './Header';
+
+
+function App(){
+  //var hola = 'Dey';
+  return (
+    <div>
+    <Header/>
+    <p>componente App(Home)</p>
+    
+    
+    </div>
+    
+  )
+}
+
+export default App;
